3c29d8411727dc3539c8c6e6e92a23a29f3624fc
yudhasj123 <yudhaamarthatungga@gmail.com>
2024-02-13 16:04:28 +0700

    menampilkan kategori di produk

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -13,4 +13,9 @@ class Produk extends Model
     {
         return $this->hasMany('App\Models\TransaksiDetail', 'produk_id');
     }
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'kategori_id');
+    }
+
 }
